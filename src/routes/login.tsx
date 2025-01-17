e435f46e7a23566f2593350a28e3d97ab8236df2
Fahro Nur Fauzi <92683779+fahrooo@users.noreply.github.com>
2025-01-17 16:25:02 +0700

    Add middleware login

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,8 +1,16 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import LoginPage from "../pages/Login";
 
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
+  beforeLoad: async () => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      throw redirect({
+        to: "/",
+      });
+    }
+  },
 });
 
 function RouteComponent() {
